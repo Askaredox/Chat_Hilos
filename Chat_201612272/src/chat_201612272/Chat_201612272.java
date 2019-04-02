243ba8355c261432a9933d2c26eca0e5f81f43ad
Askaredox <andyecarvjal@gmail.com>
2019-04-01 19:23:09 -0600

    v1.0.0

---
+++ Chat_201612272/src/chat_201612272/Chat_201612272.java
@@ -0,0 +1,10 @@
+package chat_201612272;
+/*
+ * @author ASKAR
+ */
+public class Chat_201612272 {
+    public static void main(String[] args) {
+        Window w = new Window();
+        w.setVisible(true);
+    }
+}
